a5bd10e789cb7b5bb6a6bd3d3c30c4f4f02480f2
sayamkhanraj <snsayamkhanraj@gmail.com>
2021-11-16 12:13:14 +0600

    first commit

--- src/Pages/Login/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyCsYDlGoAB0mknem3PTKXs-TNM69aU3UXw",
+    authDomain: "doctors-portal-dba32.firebaseapp.com",
+    projectId: "doctors-portal-dba32",
+    storageBucket: "doctors-portal-dba32.appspot.com",
+    messagingSenderId: "721165668841",
+    appId: "1:721165668841:web:90551d47f2c52ac70a7b78"
 };
 export default firebaseConfig;
